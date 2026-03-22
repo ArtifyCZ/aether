edf150a50a1414ea421cf335fd1ea958f9390b78
Richard Tichý <richard@tichy.io>
2026-03-22 18:06:51 +0100

    (wip)
    Signed-off-by: Richard Tichý <richard@tichy.io>

--- kernel/core/src/allocator.rs
+++ kernel/core/src/allocator.rs
@@ -6,6 +6,7 @@ use alloc::boxed::Box;
 use core::alloc::{GlobalAlloc, Layout};
 use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use kernel_hal::mmu::VirtualMemoryMappingFlags;
+use crate::interrupt_safe_spin_lock::InterruptSafeSpinLock;
 
 fn align_up(v: usize, a: usize) -> usize {
     if a == 0 {
@@ -15,20 +16,20 @@ fn align_up(v: usize, a: usize) -> usize {
     (v + mask) & !mask
 }
 
+pub struct Allocator(InterruptSafeSpinLock<AllocatorInner>);
+
 #[repr(align(16))]
-pub struct Allocator {
-    next_available_alloc_vaddr: AtomicUsize,
-    mapped_memory_end_vaddr: AtomicUsize,
-    mapping_additional_memory: AtomicBool,
+struct AllocatorInner {
+    next_available_alloc_vaddr: usize,
+    mapped_memory_end_vaddr: usize,
 }
 
 impl Allocator {
     pub unsafe fn init() -> &'static Self {
-        Box::leak(Box::new(Allocator {
-            next_available_alloc_vaddr: AtomicUsize::new(KERNEL_HEAP_BASE),
-            mapped_memory_end_vaddr: AtomicUsize::new(KERNEL_HEAP_BASE),
-            mapping_additional_memory: AtomicBool::new(false),
-        }))
+        Box::leak(Box::new(Allocator(InterruptSafeSpinLock::new(AllocatorInner {
+            next_available_alloc_vaddr: KERNEL_HEAP_BASE,
+            mapped_memory_end_vaddr: KERNEL_HEAP_BASE,
+        }))))
     }
 }
 
@@ -37,36 +38,23 @@ unsafe impl GlobalAlloc for Allocator {
         let size = layout.size();
         let align = layout.align();
 
-        loop {
-            let start = self.next_available_alloc_vaddr.load(Ordering::Acquire);
-            let end = self.mapped_memory_end_vaddr.load(Ordering::Relaxed);
-            let aligned_start = align_up(start, align);
-            let next_available_vaddr = aligned_start + size;
-            if next_available_vaddr >= end {
-                // Not enough mapped memory
-                while self.mapping_additional_memory.load(Ordering::Acquire) {
-                    core::hint::spin_loop();
-                }
-                let pages_count = align_up(size, PAGE_FRAME_SIZE) + 2;
-                unsafe {
-                    self.expand(pages_count);
-                }
-                self.mapping_additional_memory.store(false, Ordering::Release);
-                continue;
-            }
-            if self
-                .next_available_alloc_vaddr
-                .compare_exchange(
-                    start,
-                    next_available_vaddr,
-                    Ordering::Release,
-                    Ordering::Relaxed,
-                )
-                .is_ok()
-            {
-                return aligned_start as *mut u8;
+        let mut inner = self.0.lock();
+        let start = align_up(inner.next_available_alloc_vaddr, align);
+        let next_available_vaddr = start + size;
+        {
+            let end = inner.mapped_memory_end_vaddr;
+            if end <= next_available_vaddr {
+                let pages_count = align_up(size, PAGE_FRAME_SIZE) + 8;
+                inner.expand(pages_count);
             }
         }
+
+        let end = inner.mapped_memory_end_vaddr;
+        assert!(end > next_available_vaddr, "It should have been expanded!");
+
+        inner.next_available_alloc_vaddr = next_available_vaddr;
+
+        start as *mut u8
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
@@ -74,15 +62,15 @@ unsafe impl GlobalAlloc for Allocator {
     }
 }
 
-impl Allocator {
-    unsafe fn expand(&self, pages_count: usize) {
-        let base = self.mapped_memory_end_vaddr.load(Ordering::Acquire);
+impl AllocatorInner {
+    unsafe fn expand(&mut self, pages_count: usize) {
+        let base = self.mapped_memory_end_vaddr;
         for i in 0..pages_count {
             let page_vaddr = VirtualPageAddress::new(base + i * PAGE_FRAME_SIZE).unwrap();
             let phys = unsafe { PhysicalMemoryManager::alloc_frame() }.unwrap();
             let context = unsafe { VirtualMemoryManagerContext::get_kernel_context() };
             unsafe { context.map_page(page_vaddr, phys, VirtualMemoryMappingFlags::PRESENT | VirtualMemoryMappingFlags::WRITE) }.unwrap();
         }
-        self.mapped_memory_end_vaddr.store(base + pages_count * PAGE_FRAME_SIZE, Ordering::Release);
+        self.mapped_memory_end_vaddr = base + pages_count * PAGE_FRAME_SIZE;
     }
 }
