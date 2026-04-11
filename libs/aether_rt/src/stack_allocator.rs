f86e8f1575cc2474569cd14e22bdadcaa34fae0d
Richard Tichý <richard@tichy.io>
2026-04-11 15:26:07 +0200

    feat: add stack allocator to aether rt
    Signed-off-by: Richard Tichý <richard@tichy.io>

---
+++ libs/aether_rt/src/stack_allocator.rs
@@ -0,0 +1,87 @@
+use core::{
+    arch::asm,
+    ptr::null_mut,
+    sync::atomic::{AtomicPtr, AtomicUsize, Ordering},
+};
+
+static NEXT_AVAILABLE_STACK_TOP: AtomicPtr<u8> = AtomicPtr::new(null_mut());
+
+/// # SAFETY
+///
+/// Must be called exactly once, during in the init phase of a process.
+///
+/// The stack after this call must be still in the highest page of the initial stack.
+pub unsafe fn init(stack_size: usize) {
+    let highest_stack_page = unsafe {
+        let sp: *mut u8;
+        #[cfg(target_arch = "x86_64")]
+        asm!(
+            "mov {}, rsp",
+            out(reg) sp,
+            options(nomem, nostack),
+        );
+        #[cfg(target_arch = "aarch64")]
+        asm!(
+            "mov {}, sp",
+            out(reg) sp,
+            options(nomem, nostack),
+        );
+        sp.map_addr(|sp| sp & (!0xfff))
+    };
+
+    let stack_base = unsafe { highest_stack_page.byte_sub(stack_size).byte_add(4096) };
+    let next_available_stack_top = unsafe { stack_base.byte_sub(4096) }; // Empty stack guard page
+    assert_eq!(
+        next_available_stack_top.addr() % 4096,
+        0,
+        "Next available stack top is not page-aligned!",
+    );
+    NEXT_AVAILABLE_STACK_TOP.store(next_available_stack_top, Ordering::Relaxed);
+}
+
+fn alloc_stack_addr_range(stack_size: usize) -> *mut u8 {
+    loop {
+        let stack_top = NEXT_AVAILABLE_STACK_TOP.load(Ordering::Acquire);
+        if stack_top.is_null() {
+            panic!("The stack allocator hasn't been initialized!");
+        }
+        let stack_base = unsafe {
+            stack_top
+                .byte_sub(stack_size)
+                .map_addr(|stack_base| stack_base & (!0xFFF))
+        };
+        let next_available_stack_top = unsafe { stack_base.byte_sub(4096) }; // Empty stack guard page
+        if NEXT_AVAILABLE_STACK_TOP
+            .compare_exchange(
+                stack_top,
+                next_available_stack_top,
+                Ordering::Release,
+                Ordering::Relaxed,
+            )
+            .is_ok()
+        {
+            return stack_base;
+        }
+    }
+}
+
+/// Allocates a new stack of the given size and returns the new stack's top address
+/// (the next byte after the last included)
+pub fn allocate(stack_size: usize) -> *mut u8 {
+    let stack_base = alloc_stack_addr_range(stack_size);
+    let stack_base = unsafe {
+        aether_sys::sys_proc_mmap(
+            0,
+            stack_base,
+            stack_size as *mut u8,
+            aether_sys::SYS_PROT_READ | aether_sys::SYS_PROT_WRITE,
+            0,
+        )
+        .unwrap()
+    };
+    if stack_base.addr() < 0x400000 {
+        panic!("Mmap failed or returned invalid address!");
+    }
+    let stack_top = unsafe { stack_base.byte_add(stack_size) };
+    stack_top
+}
