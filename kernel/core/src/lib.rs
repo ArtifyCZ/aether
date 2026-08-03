5fa15432066147f96da65502960d6df052330172
Richard Tichý <richard@tichy.io>
2026-08-03 02:49:05 +0200

    feat: add paged memory regions iterator
    Signed-off-by: Richard Tichý <richard@tichy.io>

--- kernel/core/src/lib.rs
+++ kernel/core/src/lib.rs
@@ -3,7 +3,7 @@
 mod logger;
 pub mod memory_regions;
 
-use crate::memory_regions::MemoryRegion;
+use crate::memory_regions::{IntoPagedMemoryRegionIterator, MemoryRegion, MemoryRegionKind};
 use atom_hal::framebuffer_console::Framebuffer;
 use atom_hal::serial_console::SerialConsoleConfig;
 use log::{info, warn};
@@ -11,15 +11,20 @@ use log::{info, warn};
 pub fn bsp_main(
     framebuffer: Option<Framebuffer>,
     serial_console_config: Option<SerialConsoleConfig>,
-    memory_regions: impl Iterator<Item = MemoryRegion>,
+    memory_regions: impl Iterator<Item = MemoryRegion> + Clone,
 ) -> ! {
     logger::init(framebuffer, serial_console_config);
     info!("Booting Atom kernel...");
     info!("Bootstrap core starting...");
     warn!("Hello warning world!");
-    for memory_region in memory_regions {
+    for memory_region in memory_regions.clone() {
         info!("Memory region: {:x?}", memory_region);
     }
+    let pages_count = memory_regions
+        .filter(|region| region.kind == MemoryRegionKind::Usable)
+        .into_paged_iter()
+        .count();
+    warn!("Pages: {}", pages_count);
     warn!("Continuing...");
     let chars = ['a', 'b', 'c', 'd', 'e', 'f'];
     for i in 0..150 {
--- kernel/core/src/memory_regions.rs
+++ kernel/core/src/memory_regions.rs
@@ -1,4 +1,16 @@
-#[derive(Debug)]
+use core::iter::Peekable;
+
+/// Huge page size in bytes
+///
+/// A huge page is 1 GiB
+pub const HUGE_PAGE_SIZE: usize = 512 * 512 * 4096;
+
+/// Large page size in bytes
+///
+/// A large page is 2 MiB
+pub const LARGE_PAGE_SIZE: usize = 512 * 4096;
+
+#[derive(Debug, Clone)]
 pub struct MemoryRegion {
     pub base_address: usize,
     pub length: usize,
@@ -17,4 +29,96 @@ pub enum MemoryRegionKind {
     Reserved,
     /// Should not be used, the memory is not reliable
     BadMemory,
+}
+
+#[derive(Debug)]
+pub struct MemoryPage<const SIZE: usize> {
+    pub base_address: usize,
+}
+
+fn align_up(n: usize, align: usize) -> usize {
+    (n + align - 1) & !(align - 1)
+}
+
+fn is_aligned(n: usize, align: usize) -> bool {
+    n & (align - 1) == 0
+}
+
+#[derive(Debug)]
+pub enum MemoryRegionPage {
+    Huge(MemoryPage<HUGE_PAGE_SIZE>),
+    Large(MemoryPage<LARGE_PAGE_SIZE>),
+}
+
+#[derive(Debug, Clone)]
+pub struct PagedMemoryRegionIterator<TIter: Iterator<Item = MemoryRegion>> {
+    iter: Peekable<TIter>,
+    current_region_end: usize,
+    current_page_start: usize,
+}
+
+pub trait IntoPagedMemoryRegionIterator: Iterator<Item = MemoryRegion> + Sized {
+    fn into_paged_iter(self) -> PagedMemoryRegionIterator<Self>;
+}
+
+impl<TIter> IntoPagedMemoryRegionIterator for TIter
+where
+    TIter: Iterator<Item = MemoryRegion>,
+{
+    fn into_paged_iter(self) -> PagedMemoryRegionIterator<Self> {
+        PagedMemoryRegionIterator {
+            iter: self.peekable(),
+            current_page_start: 0,
+            current_region_end: 0,
+        }
+    }
+}
+
+impl<TIter> Iterator for PagedMemoryRegionIterator<TIter>
+where
+    TIter: Iterator<Item = MemoryRegion>,
+{
+    type Item = MemoryRegionPage;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(next_region) = self.iter.peek()
+                && (next_region.base_address <= self.current_region_end
+                    || self.current_page_start >= self.current_region_end)
+            {
+                let next_region = self.iter.next()?;
+                if self.current_page_start >= self.current_region_end {
+                    self.current_page_start = align_up(next_region.base_address, LARGE_PAGE_SIZE);
+                }
+                self.current_region_end = next_region.base_address + next_region.length;
+                continue;
+            }
+
+            assert_ne!(self.current_page_start, 0);
+
+            if is_aligned(self.current_page_start, HUGE_PAGE_SIZE)
+                && self.current_page_start + HUGE_PAGE_SIZE <= self.current_region_end
+            {
+                let current_page = self.current_page_start;
+                self.current_page_start += HUGE_PAGE_SIZE;
+                return Some(MemoryRegionPage::Huge(MemoryPage {
+                    base_address: current_page,
+                }));
+            }
+
+            assert!(is_aligned(self.current_page_start, LARGE_PAGE_SIZE));
+
+            if self.current_page_start + LARGE_PAGE_SIZE <= self.current_region_end {
+                let current_page = self.current_page_start;
+                self.current_page_start += LARGE_PAGE_SIZE;
+                return Some(MemoryRegionPage::Large(MemoryPage {
+                    base_address: current_page,
+                }));
+            }
+
+            let next_region = self.iter.next()?;
+            self.current_page_start = align_up(next_region.base_address, LARGE_PAGE_SIZE);
+            self.current_region_end = next_region.base_address + next_region.length;
+        }
+    }
 }
